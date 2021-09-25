4bdb493c1a35290bb9f923df52a2bb7d23d98cdc
Eric Little <reallyimeric@gmail.com>
2021-09-25 13:35:54 +0800

    chore: lint & ts config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 // CloudFlare API token
 // for zone: example.com (Edit)
-const CLOUDFLARE_API_TOKEN = "TO_BE_FILLED";
+const CLOUDFLARE_API_TOKEN = 'TO_BE_FILLED';
 // Currently, there seems not to be a way to get zone ids with tokens. So hardcode it here.
 // https://community.cloudflare.com/t/bug-in-list-zones-endpoint-when-using-api-token/115048
 // Trailing dots CANNOT BE OMITTED.
 const ZONES = {
-  "example.org.": {
-    id: "TO_BE_FILLED"
+  'example.org.': {
+    id: 'TO_BE_FILLED',
   },
 };
 // Arbitrary API tokens, mapping domains to customizable tokens, which can be any text.
 // Tokens for a domain can always be used to access its direct or indirect subdomains.
 // Trailing dots CANNOT BE OMITTED.
 const TOKENS = /* TOKENS: */ {
-  "example.org.": "b69541a8-0acd-4e14-aecf-053d5e1b923d",
-  "foo.example.org.": "144e7c59-7c2b-4360-9d16-00bbad549fb9",
-  "foo.bar.example.org.": "218fa52e-d3c9-419d-a800-d8510e899a30",
-} /* :TOKENS */;
+  'example.org.': 'b69541a8-0acd-4e14-aecf-053d5e1b923d',
+  'foo.example.org.': '144e7c59-7c2b-4360-9d16-00bbad549fb9',
+  'foo.bar.example.org.': '218fa52e-d3c9-419d-a800-d8510e899a30',
+};
 // Time To Live in DNS record. 1 indictes automatic.
 const TTL = 1;
 
@@ -25,12 +25,12 @@ class Flare {
     this.api_token = api_token;
   }
 
-  async request(method, path, data) {
-    const response = await fetch("https://api.cloudflare.com/client/v4/" + path, {
-      method: method,
+  async request(method: 'GET' | 'POST' | 'PUT' | 'DELETE', path: string, data: any) {
+    const response = await fetch(`https://api.cloudflare.com/client/v4/${path}`, {
+      method,
       headers: {
-        "Authorization": `Bearer ${this.api_token}`,
-        "Content-Type": "application/json"
+        Authorization: `Bearer ${this.api_token}`,
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     });
@@ -39,26 +39,25 @@ class Flare {
     }
     try {
       return await response.json();
-    }
-    catch (e) {
-      throw new Error("Upstream invalid response");
+    } catch (e) {
+      throw new Error('Upstream invalid response');
     }
   }
 
   get(path, data) {
-    return this.request("GET", path, data);
+    return this.request('GET', path, data);
   }
 
   post(path, data) {
-    return this.request("POST", path, data);
+    return this.request('POST', path, data);
   }
 
   put(path, data) {
-    return this.request("PUT", path, data);
+    return this.request('PUT', path, data);
   }
 
   delete(path, data) {
-    return this.request("delete", path, data);
+    return this.request('delete', path, data);
   }
 }
 
@@ -66,9 +65,9 @@ const flare = new Flare(CLOUDFLARE_API_TOKEN);
 
 class ClientError extends Error { }
 
-addEventListener('fetch', event => {
+addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request));
-})
+});
 
 /**
  * Respond to the request
@@ -76,29 +75,26 @@ addEventListener('fetch', event => {
  */
 async function handleRequest(request) {
   const url = new URL(request.url);
-  if (url.pathname === "/") {
-    return new Response("cfapi-ddns-worker.js is up");
+  if (url.pathname === '/') {
+    return new Response('cfapi-ddns-worker.js is up');
   }
   try {
-    const path = url.pathname.startsWith("/") ? url.pathname.substring(1) : url.pathname;
-    const [action, token, domain, rtype, content] = path.split("/", 5);
-    if (["update", "create", "delete", "upsert"].includes(action)) {
+    const path = url.pathname.startsWith('/') ? url.pathname.substring(1) : url.pathname;
+    const [action, token, domain, rtype, content] = path.split('/', 5);
+    if (['update', 'create', 'delete', 'upsert'].includes(action)) {
       if ([token, domain, rtype, content].some((e) => e === undefined)) {
-        throw new ClientError("Parameters Missing");
+        throw new ClientError('Parameters Missing');
       }
       return await handleAction(action, token, domain, rtype, content);
     }
-    else {
-      return new Response(`Unsupported action: ${action}`, { status: 404 });
-    }
-  }
-  catch (e) {
+
+    return new Response(`Unsupported action: ${action}`, { status: 404 });
+  } catch (e) {
     if (e instanceof ClientError) {
       return new Response(e, { status: 400 });
     }
-    else {
-      return new Response(e, { status: 500 });
-    }
+
+    return new Response(e, { status: 500 });
   }
 }
 
@@ -106,38 +102,36 @@ async function handleAction(action, token, domain, rtype, content) {
   const canon_domain = canonicalizeDomain(domain);
   const effective_domain = getEffectiveDomain(token, canon_domain);
   if (effective_domain === undefined) {
-    throw new ClientError("Invalid token");
+    throw new ClientError('Invalid token');
   }
-  let { id: zone_id } = getZoneByDomain(effective_domain);
+  const { id: zone_id } = getZoneByDomain(effective_domain);
   if (zone_id === undefined) {
     throw new ClientError(`Zone ID has not been specified for the domain ${effective_domain}`);
   }
   const records = (await flare.get(`zones/${zone_id}/dns_records?name=${simplifyDomain(domain)}&type=${rtype}`)).result;
-  if (action === "upsert" && records.length === 0) {
-    action = "create";
-  }
-  else {
-    action = "update";
+  if (action === 'upsert' && records.length === 0) {
+    action = 'create';
+  } else {
+    action = 'update';
   }
   let result;
   switch (action) {
-    case "create":
+    case 'create':
       if (records.length > 0) {
-        throw new ClientError("Record Already Existing");
+        throw new ClientError('Record Already Existing');
       }
-      result = await flare.post(`zones/${zone_id}/dns_records`, { type: rtype, name: simplifyDomain(domain), content: content });
+      result = await flare.post(`zones/${zone_id}/dns_records`, { type: rtype, name: simplifyDomain(domain), content });
       break;
-    case "update":
-    case "delete":
+    case 'update':
+    case 'delete':
       if (records.length > 1) {
-        throw new ClientError("Record Not Unique");
-      }
-      else if (records.length == 0) {
-        throw new ClientError("Record Not Found")
+        throw new ClientError('Record Not Unique');
+      } else if (records.length == 0) {
+        throw new ClientError('Record Not Found');
       }
       const record_id = records[0].id;
-      if (action === "update") {
-        result = await flare.put(`zones/${zone_id}/dns_records/${record_id}`, { type: rtype, name: simplifyDomain(domain), content: content });
+      if (action === 'update') {
+        result = await flare.put(`zones/${zone_id}/dns_records/${record_id}`, { type: rtype, name: simplifyDomain(domain), content });
       } else { // delete
         result = await flare.delete(`zones/${zone_id}/dns_records/${record_id}`);
       }
@@ -177,32 +171,31 @@ function getEffectiveDomain(token, domain) {
 // Generator function yielding a canonicalized domain and all of its parent-domains.
 // e.g. parentDomains("www.example.org.") -> ["www.example.org.", example.org., org.]*
 function* parentDomains(domain) {
-  if (domain !== ".") {
+  if (domain !== '.') {
     let dot = -1;
     do {
       domain = domain.substring(dot + 1);
       yield domain;
-      dot = domain.indexOf(".");
+      dot = domain.indexOf('.');
     }
-    while (dot != domain.length - 1)
+    while (dot != domain.length - 1);
   }
-  yield ".";
+  yield '.';
 }
 
 // Strip the trailing dot indicating root domain, if it is present.
 // Necessary because the poor CloudFlare cannot understand canonicalized domains.
 function simplifyDomain(domain) {
-  if (domain.endsWith(".")) {
+  if (domain.endsWith('.')) {
     domain = domain.substring(0, domain.length - 1);
   }
   return domain;
 }
 
-// Canonicalize the domain by add trailing dot indicating root domain, if it is not present. 
+// Canonicalize the domain by add trailing dot indicating root domain, if it is not present.
 function canonicalizeDomain(domain) {
-  if (!domain.endsWith(".")) {
-    domain += ".";
+  if (!domain.endsWith('.')) {
+    domain += '.';
   }
   return domain;
 }
-
