9113460f66b63e74a42cb4d6a7666fa3faa36290
Eric Little <reallyimeric@gmail.com>
2021-09-27 19:27:06 +0800

    refactor: use types from cf

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,12 +1,4 @@
-declare function addEventListener(
-  type: 'fetch',
-  listener: (evt: FetchEvent) => void,
-  options?: boolean | AddEventListenerOptions,
-): void;
-
 declare const ENV_DEBUG_MODE: string;
 declare const ENV_USE_KV: string;
 declare const ENV_CLOUDFLARE_API_TOKEN: string;
-declare namespace CFAPI_DDNS_WORKER_STORE {
-  async function get(s: string): string;
-}
+declare const CFAPI_DDNS_WORKER_STORE: KVNamespace;
